32dd49389cf34b3b15b362d70e4d5323e1482c22
MaraGheorghe <mara.gheorghe@stud.ubbcluj.ro>
2022-03-02 13:51:47 +0200

    Added a methon in RepositoryInterfaceUser

--- src/main/java/repository/RepositoryInterfaceUser.java
+++ src/main/java/repository/RepositoryInterfaceUser.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 public interface RepositoryInterfaceUser extends RepositoryInterface<User> {
 
     Optional<User> findUserByUsername(String username);
+
+    Optional<User> checkConnection(String username, String password);
 }
