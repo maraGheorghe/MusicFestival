15f01fc9d257337325569115c3e5285657430e10
MaraGheorghe <mara.gheorghe@stud.ubbcluj.ro>
2022-04-21 12:52:25 +0300

    GRPC

--- Server/src/main/java/StartRpcServer.java
+++ Server/src/main/java/StartRpcServer.java
@@ -1,6 +1,5 @@
-import network.utils.AbstractServer;
-import network.utils.RpcConcurrentServer;
-import network.utils.ServerException;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import repository.RepositoryPerformance;
@@ -9,55 +8,61 @@ import repository.RepositoryUser;
 import repository.interfaces.RepositoryInterfacePerformance;
 import repository.interfaces.RepositoryInterfaceTicket;
 import repository.interfaces.RepositoryInterfaceUser;
-import server.ServiceImplementation;
-import services.ServiceInterface;
 
 import java.io.IOException;
 import java.util.Properties;
 
 public class StartRpcServer {
-    private static int defaultPort = 22126;
-    private static final Logger logger = LogManager.getLogger();
+    private static int defaultPort = 26122;
+    private Server server;
+  //  private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
         Properties properties = new Properties();
         try {
             properties.load(StartRpcServer.class.getResourceAsStream("/server.properties"));
-            logger.info("Server properties set.");
+       //     logger.info("Server properties set.");
             System.out.println("Server properties set.");
             properties.list(System.out);
         } catch (IOException e) {
-            logger.error(e);
+       //     logger.error(e);
             System.err.println("Cannot find server.properties: " + e);
             return;
         }
         RepositoryInterfaceUser repositoryUser = new RepositoryUser(properties);
         RepositoryInterfacePerformance repositoryPerformance = new RepositoryPerformance(properties);
         RepositoryInterfaceTicket repositoryTicket = new RepositoryTicket(properties);
-        ServiceInterface serverImplementation = new ServiceImplementation(repositoryUser, repositoryPerformance, repositoryTicket);
-        int serverPort = defaultPort;
+        ServiceImplementation service = new ServiceImplementation(repositoryUser, repositoryPerformance, repositoryTicket);
         try {
-            serverPort = Integer.parseInt(properties.getProperty("server.port"));
+            defaultPort = Integer.parseInt(properties.getProperty("server.port"));
         } catch (NumberFormatException nef){
-            logger.error(nef);
+       //     logger.error(nef);
             System.err.println("Wrong  Port Number: " + nef.getMessage());
             System.err.println("Using default port: "+ defaultPort);
         }
-        System.out.println("Starting server on port: " + serverPort);
-        AbstractServer server = new RpcConcurrentServer(serverPort, serverImplementation);
+        System.out.println("Starting server on port: " + defaultPort);
+        StartRpcServer server = new StartRpcServer();
         try {
-            logger.info("Starting server...");
-            server.start();
-        } catch (ServerException e) {
-            logger.error(e);
-            System.err.println("Error starting the server: " + e.getMessage());
-        } finally {
-            try {
-                server.stop();
-            } catch(ServerException e){
-                logger.error(e);
-                System.err.println("Error stopping server: " + e.getMessage());
-            }
+        //    logger.info("Starting server...");
+            server.start(service);
+            server.blockUntilShutdown();
+        } catch (IOException | InterruptedException e) {
+        //    logger.error(e);
+            e.printStackTrace();
         }
+    }
+
+    public void start(ServiceImplementation service) throws IOException {
+        server = ServerBuilder
+                .forPort(defaultPort)
+                .addService(service)
+                .build()
+                .start();
+    }
+
+    public void blockUntilShutdown() throws InterruptedException {
+        if(server == null)
+            return;
+        server.awaitTermination();
     }
 }
